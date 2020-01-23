fed19b4395ddf52b8a8fb111f07a3bfdac46f1a2
collijos001 <collijos001@warren.k12.in.us>
2020-01-23 10:57:21 -0500

    New

--- script.js
+++ script.js
@@ -1,39 +1,39 @@
 let createButton = function(){
     let btn = document.createElement("BUTTON");
     btn.innerHTML = "Start";
-    btn.onclick = createBoard;
+    btn.onclick = createTable();
     let content = document.getElementById("content");
     content.appendChild(btn);
 };
 
-let createBoard = function(){
-    let board = document.createElement("BOARD");
-    let row1 = document.createElement("ROW1");
-    let row2 = document.createElement("ROW2");
-    let row3 = document.createElement("ROW3");
-    let row4 = document.createElement("ROW4");
+let createTable = function(){
+    let table = document.createElement("BOARD");
+    let row1 = document.createElement("TR");
+    let row2 = document.createElement("TR");
+    let row3 = document.createElement("TR");
+    let row4 = document.createElement("TR");
     let colHeading1 = document.createElement("TH");
     let colHeading2 = document.createElement("TH");
-    colHeading2.innerText = "L";
+    colHeading2.innerText = "";
     let colHeading3 = document.createElement("TH");
-    colHeading3.innerText = "L";
+    colHeading3.innerText = "";
     let colHeading4 = document.createElement("TH");
-    colHeading4.innerText = "L";
+    colHeading4.innerText = "";
     let rowHeading2 = document.createElement("TH");
-    rowHeading2.innerText = "L";
+    rowHeading2.innerText = "";
     let rowHeading3 = document.createElement("TH");
-    rowHeading3.innerText = "L";
+    rowHeading3.innerText = "";
     let rowHeading4 = document.createElement("TH");
-    rowHeading4.innerText = "L";
-    let data1 = document.createElement("D1");
-    let data2 = document.createElement("D2");
-    let data3 = document.createElement("D3");
-    let data4 = document.createElement("D4");
-    let data5 = document.createElement("D5");
-    let data6 = document.createElement("D6");
-    let data7 = document.createElement("D7");
-    let data8 = document.createElement("D8");
-    let data9 = document.createElement("D9");
+    rowHeading4.innerText = "";
+    let data1 = document.createElement("TD");
+    let data2 = document.createElement("TD");
+    let data3 = document.createElement("TD");
+    let data4 = document.createElement("TD");
+    let data5 = document.createElement("TD");
+    let data6 = document.createElement("TD");
+    let data7 = document.createElement("TD");
+    let data8 = document.createElement("TD");
+    let data9 = document.createElement("TD");
     data1.innerText = "XO";
     data2.innerText = "XO";
     data3.innerText = "XO";
@@ -64,15 +64,17 @@ let createBoard = function(){
     row4.appendChild(data8);
     row4.appendChild(data9);
 
-    board.appendChild(row1);
-    board.appendChild(row2);
-    board.appendChild(row3);
-    board.appendChild(row4);
+    table.appendChild(row1);
+    table.appendChild(row2);
+    table.appendChild(row3);
+    table.appendChild(row4);
 
 
     let content = document.getElementById("content");
-    content.appendChild(board);
+    content.appendChild(table);
 
 
 
 };
+
+let image = document.createElement("IMG");
